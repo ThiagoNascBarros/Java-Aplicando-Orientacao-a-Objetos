cdf5e24c9c1eb2ae583ceb66ebdb119ebab7560b
ThiagoNascBarros <thiago171nasciment@gmail.com>
2025-11-21 22:07:49 -0300

    feat: Criando classe Filme e Main
    Signed-off-by: ThiagoNascBarros <thiago171nasciment@gmail.com>

---
+++ src/Filme.java
@@ -0,0 +1,10 @@
+public class Filme {
+
+    public String Nome;
+    public int AnoDeLancamento;
+    public boolean IncluidoNoPlano;
+    public double Avaliacao;
+    public int TotalDeAvaliacoes;
+    public int DuracaoEmMinutos;
+
+}
