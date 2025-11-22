0aacd9d6ab55ffe7f08da62739a4350d911dd151
ThiagoNascBarros <thiago171nasciment@gmail.com>
2025-11-22 00:36:48 -0300

    feat: Criando métodos para listar objetos de Filme

--- src/Filme.java
+++ src/Filme.java
@@ -3,17 +3,17 @@ public class Filme {
     public String Nome;
     public int AnoDeLancamento;
     public boolean IncluidoNoPlano;
-    public double Avaliacao;
+    public double SomaDasAvaliacoes;
     public int TotalDeAvaliacoes;
     public int DuracaoEmMinutos;
 
     public Filme() { }
 
-    public Filme(String nome, int anoDeLancamento, boolean incluidoNoPlano, double avaliacao, int totalDeAvaliacoes, int duracaoEmMinutos) {
+    public Filme(String nome, int anoDeLancamento, boolean incluidoNoPlano, double somaDasAvaliacoes, int totalDeAvaliacoes, int duracaoEmMinutos) {
         Nome = nome;
         AnoDeLancamento = anoDeLancamento;
         IncluidoNoPlano = incluidoNoPlano;
-        Avaliacao = avaliacao;
+        SomaDasAvaliacoes = somaDasAvaliacoes;
         TotalDeAvaliacoes = totalDeAvaliacoes;
         DuracaoEmMinutos = duracaoEmMinutos;
     }
@@ -24,8 +24,27 @@ public class Filme {
                 "Nome = '" + Nome + '\'' +
                 ", AnoDeLancamento = " + AnoDeLancamento +
                 ", IncluidoNoPlano = " + IncluidoNoPlano +
-                ", Avaliacao = " + Avaliacao +
+                ", Avaliacao = " + SomaDasAvaliacoes +
                 ", TotalDeAvaliacoes = " + TotalDeAvaliacoes +
                 ", DuracaoEmMinutos = " + DuracaoEmMinutos;
+    }
+
+    public void exibeFichaTecnica() {
+        var verifyPlano = (!IncluidoNoPlano) ? "Não está incluido no seu plano atualmente" : "Está incluido em seu plano!";
+
+        System.out.println("Nome do Filme: " + Nome);
+        System.out.println("Ano de lançamento: " + AnoDeLancamento);
+        System.out.println("Está incluido no plano? " + verifyPlano);
+        System.out.println("Avaliação: " + SomaDasAvaliacoes);
+        System.out.println("Duração em Minutos: " + DuracaoEmMinutos);
+    }
+
+    public void avaliar(double nota) {
+        SomaDasAvaliacoes += nota;
+        TotalDeAvaliacoes++;
+    }
+
+    public double obterMediaDasAvaliacoes() {
+        return SomaDasAvaliacoes / TotalDeAvaliacoes;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,22 @@ public class Main {
 
         velozes_e_furiosos.Nome = "Velozes e furiosos";
         velozes_e_furiosos.AnoDeLancamento = 2016;
-        velozes_e_furiosos.Avaliacao = 7.7;
+        velozes_e_furiosos.SomaDasAvaliacoes = 7.7;
         velozes_e_furiosos.DuracaoEmMinutos = 160;
+        velozes_e_furiosos.IncluidoNoPlano = true;
 
-        System.out.println(velozes_e_furiosos.toString());
+        // Utilizando toString para exibir dados do Objeto Filme
+        // System.out.println(velozes_e_furiosos.toString());
 
+        velozes_e_furiosos.exibeFichaTecnica();
+        velozes_e_furiosos.avaliar(8);
+        velozes_e_furiosos.avaliar(9);
+        velozes_e_furiosos.avaliar(3);
+        velozes_e_furiosos.avaliar(10);
+
+        System.out.println("\nSoma das avaliações: " + velozes_e_furiosos.SomaDasAvaliacoes);
+        System.out.println("Total das avaliações: " + velozes_e_furiosos.TotalDeAvaliacoes);
+
+        System.out.println("\nMédia das avaliações: " + velozes_e_furiosos.obterMediaDasAvaliacoes());
     }
 }
