0a8dddc04da06298a01bd7f56d8056bc95c266d6
ThiagoNascBarros <thiago171nasciment@gmail.com>
2025-11-22 00:46:41 -0300

    feat: Criando classe Pessoa e adicionando método de OláMundo

---
+++ src/Challenges/Pessoa/Main.java
@@ -0,0 +1,11 @@
+package Challenges.Pessoa;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Construindo o objeto Thiago com base na Classe Pessoa
+        Pessoa thiago = new Pessoa("Thiago", 18);
+        thiago.OlaMundo();
+    }
+
+}
---
+++ src/Challenges/Pessoa/Pessoa.java
@@ -0,0 +1,17 @@
+package Challenges.Pessoa;
+
+public class Pessoa {
+
+    public String nome;
+    public int idade;
+
+    // Criando contrutor para a Classe Pessoa
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public void OlaMundo() {
+        System.out.println("Olá mundo, eu sou " + nome + " e tenho " + idade + " anos.");
+    }
+}
