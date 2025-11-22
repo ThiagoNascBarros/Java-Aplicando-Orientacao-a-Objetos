8e741863de0ed5bdd4fceaf64c3cb8d32b73f996
ThiagoNascBarros <thiago171nasciment@gmail.com>
2025-11-21 23:46:16 -0300

    feat: Criando construtores para instânciar Objetos e método toString para leitura dos Objetos instânciados

--- src/Filme.java
+++ src/Filme.java
@@ -7,4 +7,25 @@ public class Filme {
     public int TotalDeAvaliacoes;
     public int DuracaoEmMinutos;
 
+    public Filme() { }
+
+    public Filme(String nome, int anoDeLancamento, boolean incluidoNoPlano, double avaliacao, int totalDeAvaliacoes, int duracaoEmMinutos) {
+        Nome = nome;
+        AnoDeLancamento = anoDeLancamento;
+        IncluidoNoPlano = incluidoNoPlano;
+        Avaliacao = avaliacao;
+        TotalDeAvaliacoes = totalDeAvaliacoes;
+        DuracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    @Override
+    public String toString() {
+        return "Filme\n" +
+                "Nome = '" + Nome + '\'' +
+                ", AnoDeLancamento = " + AnoDeLancamento +
+                ", IncluidoNoPlano = " + IncluidoNoPlano +
+                ", Avaliacao = " + Avaliacao +
+                ", TotalDeAvaliacoes = " + TotalDeAvaliacoes +
+                ", DuracaoEmMinutos = " + DuracaoEmMinutos;
+    }
 }
