ac5b65cf5d4278c4967d891527a4c0a79e6c8742
ThiagoNascBarros <thiago171nasciment@gmail.com>
2025-11-21 23:47:33 -0300

    feat: Instânciando Objeto e atribuindo valores para as propriedades

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
 public class Main {
+    public static void main(String[] args) {
+        Filme velozes_e_furiosos = new Filme();
+
+        velozes_e_furiosos.Nome = "Velozes e furiosos";
+        velozes_e_furiosos.AnoDeLancamento = 2016;
+        velozes_e_furiosos.Avaliacao = 7.7;
+        velozes_e_furiosos.DuracaoEmMinutos = 160;
+
+        System.out.println(velozes_e_furiosos.toString());
+
+    }
 }
