1537e83d8ddb83da71b6c85109bf80e66e463e58
ThiagoNascBarros <thiago171nasciment@gmail.com>
2025-11-22 21:39:05 -0300

    feat: Criando classe Aluno e atribuindo métodos de Exibir Informações e Calcular Idade

---
+++ src/Challenges/Aluno/Aluno.java
@@ -0,0 +1,34 @@
+package Challenges.Aluno;
+
+import Challenges.Carro.Carro;
+
+class Main {
+
+    public static void main(String[] args) {
+        Aluno thiago = new Aluno();
+
+        thiago.Nome = "Thiago do nascimento barros";
+        thiago.AnoDeNascimento = 2007;
+
+        thiago.exibirInformacoes(thiago);
+    }
+
+}
+
+public class Aluno {
+
+    public String Nome;
+    public int AnoDeNascimento;
+
+    public void exibirInformacoes(Aluno aluno) {
+        System.out.println("========== Dados do Aluno ==========");
+        System.out.println("Nome: " + aluno.Nome);
+        System.out.println("Idade: " + aluno.calcularIdade(aluno.AnoDeNascimento));
+        System.out.println("====================================");
+    }
+
+    public int calcularIdade(int AnoDeNascimento) {
+        return 2025 - AnoDeNascimento;
+    }
+
+}
